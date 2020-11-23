451290e9aa2b78b13065f9560e5602c333ae3626
dhruvsinghal34 <drmonikagupta16@gmail.com>
2020-11-23 17:33:40 +0530

    bar

---
+++ screens/QRcodeScanner.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Text, View,StyleSheet,TouchableOpacity} from 'react-native';
+import * as  Permissions from 'expo-permissions'
+import BarCodeScanner from 'expo-barcode-scanner'
+
+export default class QRcode extends React.Component {
+    constructor (){
+        super();
+        this.state={
+        hasCameraPermissions:null,
+        scanned:false,
+        scannedData:'',
+        buttonState:"normal"
+        }
+    }
+
+    getCameraPermissions= async () => {
+      const {status}=await Permissions.askAsync (Permissions.CAMERA)
+      this.setState({
+          hasCameraPermissions:status==="granted",
+          buttonState:"clicked",
+          scanned:false
+
+      })
+    }
+    handlerBarCodeScanner = async ({type,data})=>{
+      this.setState ({
+          scanned:true,
+          scannedData:data,
+          buttonState:"normal"
+      })
+    }
+render(){
+    const hasCameraPermissions = this.state.hasCameraPermissions;
+    const scanned  = this.state.scanned;
+    const buttonState = this.state.buttonState;
+
+    if (buttonState === 'clicked'&& hasCameraPermissions){
+    return(
+        <BarCodeScanner
+        onBarCodeScanned={scanned?undefined:this.handlerBarCodeScanner}
+        ></BarCodeScanner>
+    )
+    }
+else if (buttonState === "normal"){
+    return(
+        <View style={styles.container}>
+      <Text style={styles.displayText}>{hasCameraPermissions===true?this.state.scannedData:"Request camera Permissions"}</Text>
+        
+        <TouchableOpacity style={styles.scanButton}
+        onPress={this.getCameraPermissions}
+        ><Text style={styles.buttonText}>QR code scanner </Text>
+        </TouchableOpacity>
+        </View>
+      )
+      }
+      }
+}
+
+
+const styles = StyleSheet.create({ 
+    container: { flex: 1, 
+      justifyContent: 'center',
+       alignItems: 'center' 
+      },
+   displayText:{ 
+    fontSize: 15, 
+  textDecorationLine: 'underline' 
+}, 
+ scanButton:{ 
+backgroundColor: '#2196F3', 
+   padding: 10, 
+   margin: 10 
+},
+   buttonText:{
+  fontSize:20,
+}
+  })
